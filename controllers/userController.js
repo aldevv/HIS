639667e382d9b196ee2d50ab9fd76b2aa22dcf52
Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2021-09-08 21:24:43 -0500

    Solved conflicts with edit-user branch

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,16 +3,15 @@ const { Op } = require("sequelize");
 exports.list_users = async function (req, res) {
   if (isNaN(req.query.page)) {
     var page = 1;
-  }
-  else {
+  } else {
     var page = req.query.page;
   }
   const users = await User.findAndCountAll({
     limit: 5,
-    offset: (page - 1) * 5
+    offset: (page - 1) * 5,
   });
   //console.log("users:", JSON.stringify(users, null, 2));
-  res.render("Users", { title: "Users", users});
+  res.render("Users", { title: "Users", users });
 };
 
 exports.create_user = async function (req, res) {
@@ -21,16 +20,16 @@ exports.create_user = async function (req, res) {
 };
 
 exports.delete_multiple_users = async function (req, res) {
-  var users = JSON.parse(req.body.users)
+  var users = JSON.parse(req.body.users);
   const user = await User.destroy({
     where: {
       id: {
-        [Op.in]: users
-      }
-    }
+        [Op.in]: users,
+      },
+    },
   });
-  res.send("Hello World")
-}
+  res.send("Hello World");
+};
 
 exports.delete_user = async function (req, res) {
   const user = await User.destroy({
@@ -42,6 +41,7 @@ exports.delete_user = async function (req, res) {
 };
 
 exports.update_user = async function (req, res) {
+  console.log("mi request llego sano y salvo: ", req.body);
   var user = await User.update(req.body, {
     where: {
       id: req.params.id,
--- public/js/users.js
+++ public/js/users.js
@@ -1,4 +1,25 @@
 $(document).ready(function () {
+  // update specific user
+  $("#editModal").on("show.bs.modal", function (event) {
+    var button = $(event.relatedTarget);
+    var user_id = button.data("id");
+    $("#formEdit").submit((event) => {
+      var $inputs = $("#formEdit :input");
+      var values = {};
+      $inputs.each(function () {
+        if (this.name) {
+          values[this.name] = $(this).val();
+        }
+      });
+
+      $.ajax({
+        url: `/user/${user_id}/update`,
+        type: "patch",
+        data: values,
+        dataType: "application/json",
+      });
+    });
+  });
   // Activate tooltip
   $('[data-toggle="tooltip"]').tooltip();
 
